2ac10219903064be6ce7e867261660efde37021b
Waqas Tahir <wakaztahir@gmail.com>
2021-05-27 14:08:58 +0500

    main page css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,8 @@
 "use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("react");
 var React = require("react");
@@ -7,14 +11,18 @@ var main_header_1 = require("../components/header/main-header");
 var parallax_1 = require("../components/parallax/parallax");
 var project_card_1 = require("../components/project-card/project-card");
 var main_sidebar_1 = require("../components/sidebar/main-sidebar");
-var css = require("../styles/main-page.module.scss");
 var MyApp_1 = require("../components/commons/MyApp");
 var core_1 = require("@material-ui/core");
 var MyButton_1 = require("../components/commons/MyButton");
+var styled_components_1 = require("styled-components");
+var Container = styled_components_1.default.main(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  display: flex;\n  flex-direction: column;\n\n  ", " {\n    flex-direction: row;\n  }\n"], ["\n  display: flex;\n  flex-direction: column;\n\n  ", " {\n    flex-direction: row;\n  }\n"])), function (props) { return props.theme.breakpoints.up("md"); });
+var ContentSection = styled_components_1.default.section(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  width: 100%;\n  padding: 5rem 0;\n  box-sizing: border-box;\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  align-items: center;\n"], ["\n  width: 100%;\n  padding: 5rem 0;\n  box-sizing: border-box;\n  display: flex;\n  flex-direction: column;\n  justify-content: center;\n  align-items: center;\n"])));
+var HomeSection = styled_components_1.default(ContentSection)(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n  min-height: 100vh;\n  padding: 0;\n"], ["\n  min-height: 100vh;\n  padding: 0;\n"])));
+var ProjectsSection = styled_components_1.default(ContentSection)(templateObject_4 || (templateObject_4 = __makeTemplateObject(["\n  min-height: 60vh;\n  padding-top: 0;\n\n  ", " {\n    flex-direction: row;\n    flex-wrap: wrap;\n  }\n"], ["\n  min-height: 60vh;\n  padding-top: 0;\n\n  ", " {\n    flex-direction: row;\n    flex-wrap: wrap;\n  }\n"])), function (props) { return props.theme.breakpoints.up("md"); });
 function Home() {
     var _a = react_1.useState(false), aboutDialogOpen = _a[0], setAboutDialog = _a[1];
     return (<MyApp_1.default>
-      <main className={css.container}>
+      <Container>
         <title>Waqas Tahir | Trying My Best</title>
         <core_1.Hidden mdUp={true}>
           <main_header_1.default />
@@ -26,7 +34,7 @@ function Home() {
             setAboutDialog(false);
         }}/>
         <parallax_1.default>
-          <section id="home" className={css.centered + " " + css.contentSection + " " + css.home}>
+          <HomeSection id="home">
             <h1 className="h-text">Hi , I am Waqas.</h1>
             <p>software developer</p>
             <MyButton_1.default onClick={function () {
@@ -34,36 +42,35 @@ function Home() {
         }}>
               About Me
             </MyButton_1.default>
-          </section>
-          <section id="projects" className={css.contentSection + " " + css.projects}>
-            <div className={css.projects}>
-              <project_card_1.default tags={["March 2021"]}>
-                <project_card_1.ProjectCardTitle>Timeline</project_card_1.ProjectCardTitle>
-                <project_card_1.ProjectCardDescription>
-                  A Material Design Web App inspired by Google Keep to make notes and organize
-                  your information , Data
-                  is synced to google drive.
-                </project_card_1.ProjectCardDescription>
-                <project_card_1.ProjectCardLink href="https://timeline-notes.github.io/web">
-                  Live Demo
-                </project_card_1.ProjectCardLink>
-              </project_card_1.default>
-              <project_card_1.default tags={["August 2020"]}>
-                <project_card_1.ProjectCardTitle>Acadtable</project_card_1.ProjectCardTitle>
-                <project_card_1.ProjectCardDescription>
-                  Online <strong>react</strong> application that allows the users
-                  to make academic time tables.
-                  <br />
-                  This is a block based table generator application.
-                </project_card_1.ProjectCardDescription>
-                <project_card_1.ProjectCardLink href="https://wakaztahir.github.io/acadtable/">
-                  Live Demo
-                </project_card_1.ProjectCardLink>
-              </project_card_1.default>
-            </div>
-          </section>
+          </HomeSection>
+          <ProjectsSection id="projects">
+            <project_card_1.default tags={["March 2021"]}>
+              <project_card_1.ProjectCardTitle>Timeline</project_card_1.ProjectCardTitle>
+              <project_card_1.ProjectCardDescription>
+                A Material Design Web App inspired by Google Keep to make notes and organize
+                your information , Data
+                is synced to google drive.
+              </project_card_1.ProjectCardDescription>
+              <project_card_1.ProjectCardLink href="https://timeline-notes.github.io/web">
+                Live Demo
+              </project_card_1.ProjectCardLink>
+            </project_card_1.default>
+            <project_card_1.default tags={["August 2020"]}>
+              <project_card_1.ProjectCardTitle>Acadtable</project_card_1.ProjectCardTitle>
+              <project_card_1.ProjectCardDescription>
+                Online <strong>react</strong> application that allows the users
+                to make academic time tables.
+                <br />
+                This is a block based table generator application.
+              </project_card_1.ProjectCardDescription>
+              <project_card_1.ProjectCardLink href="https://wakaztahir.github.io/acadtable/">
+                Live Demo
+              </project_card_1.ProjectCardLink>
+            </project_card_1.default>
+          </ProjectsSection>
         </parallax_1.default>
-      </main>
+      </Container>
     </MyApp_1.default>);
 }
 exports.default = Home;
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,50 @@ import ProjectCard, {
   ProjectCardDescription, ProjectCardLink, ProjectCardTitle
 } from "../components/project-card/project-card"
 import MainSidebar from "../components/sidebar/main-sidebar"
-import * as css from "../styles/main-page.module.scss"
 import MyApp from "../components/commons/MyApp"
 import { Hidden, Typography } from "@material-ui/core"
 import MyButton from "../components/commons/MyButton"
 import styled from "styled-components"
 
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  ${props => props.theme.breakpoints.up("md")} {
+    flex-direction: row;
+  }
+`
+
+const ContentSection = styled.section`
+  width: 100%;
+  padding: 5rem 0;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const HomeSection = styled(ContentSection)`
+  min-height: 100vh;
+  padding: 0;
+`
+
+const ProjectsSection = styled(ContentSection)`
+  min-height: 60vh;
+  padding-top: 0;
+
+  ${props => props.theme.breakpoints.up("md")} {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+`
+
 export default function Home() {
   const [aboutDialogOpen, setAboutDialog] = useState(false)
   return (
     <MyApp>
-      <main className={css.container}>
+      <Container>
         <title>Waqas Tahir | Trying My Best</title>
         <Hidden mdUp={true}>
           <MainHeader />
@@ -32,7 +65,7 @@ export default function Home() {
           }}
         />
         <Parallax>
-          <section id="home" className={css.centered + " " + css.contentSection + " " + css.home}>
+          <HomeSection id="home">
             <h1 className="h-text">Hi , I am Waqas.</h1>
             <p>software developer</p>
             <MyButton
@@ -42,36 +75,34 @@ export default function Home() {
             >
               About Me
             </MyButton>
-          </section>
-          <section id="projects" className={css.contentSection + " " + css.projects}>
-            <div className={css.projects}>
-              <ProjectCard tags={["March 2021"]}>
-                <ProjectCardTitle>Timeline</ProjectCardTitle>
-                <ProjectCardDescription>
-                  A Material Design Web App inspired by Google Keep to make notes and organize
-                  your information , Data
-                  is synced to google drive.
-                </ProjectCardDescription>
-                <ProjectCardLink href="https://timeline-notes.github.io/web">
-                  Live Demo
-                </ProjectCardLink>
-              </ProjectCard>
-              <ProjectCard tags={["August 2020"]}>
-                <ProjectCardTitle>Acadtable</ProjectCardTitle>
-                <ProjectCardDescription>
-                  Online <strong>react</strong> application that allows the users
-                  to make academic time tables.
-                  <br />
-                  This is a block based table generator application.
-                </ProjectCardDescription>
-                <ProjectCardLink href="https://wakaztahir.github.io/acadtable/">
-                  Live Demo
-                </ProjectCardLink>
-              </ProjectCard>
-            </div>
-          </section>
+          </HomeSection>
+          <ProjectsSection id="projects">
+            <ProjectCard tags={["March 2021"]}>
+              <ProjectCardTitle>Timeline</ProjectCardTitle>
+              <ProjectCardDescription>
+                A Material Design Web App inspired by Google Keep to make notes and organize
+                your information , Data
+                is synced to google drive.
+              </ProjectCardDescription>
+              <ProjectCardLink href="https://timeline-notes.github.io/web">
+                Live Demo
+              </ProjectCardLink>
+            </ProjectCard>
+            <ProjectCard tags={["August 2020"]}>
+              <ProjectCardTitle>Acadtable</ProjectCardTitle>
+              <ProjectCardDescription>
+                Online <strong>react</strong> application that allows the users
+                to make academic time tables.
+                <br />
+                This is a block based table generator application.
+              </ProjectCardDescription>
+              <ProjectCardLink href="https://wakaztahir.github.io/acadtable/">
+                Live Demo
+              </ProjectCardLink>
+            </ProjectCard>
+          </ProjectsSection>
         </Parallax>
-      </main>
+      </Container>
     </MyApp>
   )
 }
