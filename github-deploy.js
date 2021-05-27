5171537588ca6ba713f8010b076590af3da6fedd
Waqas Tahir <wakaztahir@gmail.com>
2021-05-27 21:28:41 +0500

    ready

--- github-deploy.js
+++ github-deploy.js
@@ -3,7 +3,7 @@ const ghpages = require("gh-pages")
 ghpages.publish(
   "public",
   {
-    branch: "master",
+    branch: "public",
     repo: "https://github.com/wakaztahir/wakaztahir.github.io.git",
   },
   () => {
